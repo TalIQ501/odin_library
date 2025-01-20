be6851f7bb73a99feee77f6697ca1277c0c2f666
TalIq501 <talhaiqbal501@gmail.com>
2025-01-20 21:37:02 +0500

    Add book dialog created

--- main.js
+++ main.js
@@ -39,6 +39,26 @@ class Book {
 }
 
 const library = document.getElementById('library');
+const btnAddBook = document.getElementById('btn-add-book');
+const dialogAddBook = document.getElementById('dialog-add-book');
+const btnDialogClose = document.getElementById('btn-dialog-close');
+
+const formAddBook = document.getElementById('form-add-book');
+const addBookName = document.getElementById('add-book-name');
+const addBookAuthor = document.getElementById('add-book-author');
+const addBookYear = document.getElementById('add-book-year');
+const addBookSubmit = document.getElementById('add-book-submit');
+
+const emptyFieldsDiv = document.getElementById('empty-fields');
+
+btnAddBook.addEventListener("click", () => {
+    dialogAddBook.show();
+})
+
+btnDialogClose.addEventListener("click", () => {
+    dialogAddBook.close();
+    console.log('Clicked')
+})
 
 const bookLibrary = [];
 
@@ -49,8 +69,31 @@ const displayLibrary = () => {
     });
 }
 
-const addBookToLibrary = () => {
-    
+formAddBook.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const name = addBookName.value;
+    const author = addBookAuthor.value;
+    const year = addBookYear.value;
+
+    if (name === '' || author === '' || year === '') {
+        return emptyFields();
+    }
+
+    while (library.firstChild) {
+        library.removeChild(library.firstChild)
+    }
+
+    emptyFieldsDiv.textContent = '';
+
+    const newBook = new Book(name, author, year);
+    bookLibrary.push(newBook);
+
+    displayLibrary();
+})
+
+const emptyFields = () => {
+    emptyFieldsDiv.textContent = 'Fill out your respective fields';
 }
 
 let lotr = new Book('Lord of the Rings', 'J.R.R. Tolkien', 1950);
